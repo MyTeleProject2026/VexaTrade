a700b2cf55706f3584f0c4c9900516b0610780a5
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-06 16:19:42 +0630

    Add backend/services/vexaccount.js

---
+++ backend/services/vexaccount.js
@@ -0,0 +1,13 @@
+// backend/services/vexaccount.js
+
+module.exports = {
+  createAccount: async (data) => {
+    // TODO: implement account creation logic
+    throw new Error('createAccount not implemented');
+  },
+
+  getAccount: async (id) => {
+    // TODO: implement fetch account by id logic
+    throw new Error('getAccount not implemented');
+  },
+};
