618767a68aff2d5de460e23807457b355773ef8f
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-07 18:19:55 +0630

    Update publicFundsPlans.js

--- backend/publicFundsPlans.js
+++ backend/publicFundsPlans.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("./db");
-const { authUser } = require("./middleware/auth");
+const { authUser, authAdmin } = require('./src/middleware/auth');
 
 router.get("/", authUser, async (req, res, next) => {
   try {
