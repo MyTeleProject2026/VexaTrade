aa9b3de3ec55745e4ed77f747bbc66be8448ef11
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-10 04:19:30 +0630

    Update transferRoutes.js

--- backend/src/routes/transferRoutes.js
+++ backend/src/routes/transferRoutes.js
@@ -1,1 +1,25 @@
+// src/routes/transferRoutes.js
+const express = require('express');
+const router = express.Router();
+const QRCode = require('qrcode');
+const pool = require('../../db');
+const { authUser } = require('../middleware/auth');
+const { createError, createTransactionLog, createUserNotification } = require('../utils/helpers');
 
+router.get('/user/qr-code', authUser, async (req, res, next) => {
+  // same as original
+});
+
+router.get('/user/by-uid/:uid', authUser, async (req, res, next) => {
+  // same as original
+});
+
+router.post('/user/transfer', authUser, async (req, res, next) => {
+  // same as original
+});
+
+router.get('/user/transfers', authUser, async (req, res, next) => {
+  // same as original
+});
+
+module.exports = router;
