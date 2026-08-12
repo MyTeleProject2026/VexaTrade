32e78ca56e66df046de4ac790fa407ed55903023
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-12 23:47:56 +0630

    Update walletRoutes.js

--- backend/src/routes/walletRoutes.js
+++ backend/src/routes/walletRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../../db');
 const { authUser } = require('../middleware/auth');
+const storage = require('../../cloudinaryStorage');
 const { createError, toNumber } = require('../utils/helpers');
 const { getBinanceHomeMarkets } = require('../../services/tradeService');
 
