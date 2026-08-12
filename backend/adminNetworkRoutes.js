57c26ae37c4b4fffb699e5b28ef4cbdf2fe67482
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-12 23:13:22 +0630

    Update adminNetworkRoutes.js

--- backend/adminNetworkRoutes.js
+++ backend/adminNetworkRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("./db");
-
+const storage = require('../../cloudinaryStorage');
 // Import sync functions from depositVerificationService
 const { 
   syncVerificationSettingsFromWallets, 
