03fc404ee9e52ac8922411582376041a7ba30561
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-09-17 07:30:13 +0630

    fix: install financial polling guard at app startup

--- frontend-user/src/main.jsx
+++ frontend-user/src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { installFinancialPollingGuard } from "./services/financialPollingGuard";
 import "./index.css";
+
+installFinancialPollingGuard();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
